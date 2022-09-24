4944d0846a6d6a30fe69e58dc456f28c3e3cca00
warmac9 <aplikas7@gmail.com>
2022-09-24 11:22:08 +0200

    oprava prekladu

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("Presuň loptu z %d do %d\n", a, b);
+    printf("Presuň kameň z %d do %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a);
-    printf("Presuň loptu %d z %d do %d\n", n, a, b);
+    printf("Presuň kameň %d z %d do %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
